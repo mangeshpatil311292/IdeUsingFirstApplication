4a15be32a2407456dea8db75e774396fcf9ae45a
mangeshpatil311292 <mangeshpatil311292@gmail.com>
2020-10-11 01:10:38 +0530

    add stash

--- src/main/java/com/dm/demo/TestClass.java
+++ src/main/java/com/dm/demo/TestClass.java
@@ -5,6 +5,9 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Demo Add Once again");
+	
+        //I am Using git stash command
+        System.out.println("git stash");
 	}
 
 }
